a08b4ee63e4bf373b4ea8736e27093bd38b9e8fb
huang_weiqin <weiqin.huang96@gmail.com>
2023-01-06 17:15:27 +0100

    graph 1

--- io.h
+++ io.h
@@ -1,18 +1,72 @@
+#pragma once
 #include "list.h"
 #include <stdio.h>
 #include <string.h>
+
+static job_t* jobs[255];
+static job_nb = 0;
+
+job_t* get_job_by_name(char *name) {
+	for (int i = 0; i < job_nb; ++i) {
+		if (strcmp(name, jobs[i]->title) == 0) {
+			return jobs[i];
+		}
+	}
+	job_t* res = malloc(sizeof(job_t)); //开辟内存保存一个job
+	jobs[job_nb] = res;
+	job_nb += 1;
+	res->precedence = malloc(sizeof(list_t));
+	res->posteriority = malloc(sizeof(list_t));
+	strcpy(res->title, name);
+	return res;
+}
+
+int insert_to_list(list_t* list,job_t* job) {
+	elmlist_t* e = malloc(sizeof(elmlist_t));
+	e->jb = job;
+	if (list->numelm == 0) {
+		list->numelm = 1;
+		list->head = e;
+		list->tail = e;
+	}
+	else {
+		list->numelm += 1;
+		list->tail->next = e;
+		e->prev = list->tail;
+		list->tail = e;
+	}
+	return list->numelm;
+}
+
+
 list_t* read_graph(char* str) {
-	FILE* fp = fopen(str, "r+");
+	
 	char buff[255];
-	job_t* jobs[255];
+	
 	char jobs_str[255][255];
-
-
+	FILE* fp = fopen(str, "r+");
 	while (!feof(fp)) //只要还没到文件末尾，就执行循环
 	{
 		fgets(buff, 255, (FILE*)fp); //读取一行放到buff中
 		int result = split(jobs_str, buff, ' ');
+		if (result > 1) {
+			job_t* j = get_job_by_name("fuck");
+			printf("%s\n", j->title);
+			//j->life = atoi(jobs_str[1]);
+			
+			//for (int i = 2; i < result; ++i) {
+				//job_t* jp = get_job_by_name(jobs_str[i]);
+				//insert_to_list(j->precedence, jp);
+				//insert_to_list(jp->posteriority, j);
+			//}
+		}
 	}
+
+	printf("%d", job_nb);
+
+
+
+	fclose(fp);
 };
 
 
@@ -22,29 +76,31 @@ int split(char dst[][255], char* str, const char* spl)
 {
 	int str_nb = 0;
 	int char_nb = 0;
-	int prev_is_blank = 1;
+	int prev_is_char = 0;
 
 	for (int i = 0; i < 255; ++i) {
 		if (str[i] == '\0') {
-			dst[str_nb - 1][char_nb] = '\0';
+			if (prev_is_char == 1) {
+				dst[str_nb][char_nb] = '\0';
+				str_nb += 1;
+				char_nb = 0;
+			}
 			break;
 		}
 
 		if (str[i] == ' ') {
-			if (prev_is_blank == 0) {
-				dst[str_nb - 1][char_nb] = '\0';
-			}
-			prev_is_blank = 1;
-		}
-
-		if (str[i] != ' ') {
-			if (prev_is_blank == 1) {
+			if (prev_is_char == 1) {
+				dst[str_nb][char_nb] = '\0';
 				str_nb += 1;
 				char_nb = 0;
 			}
-			
-			dst[str_nb - 1][char_nb++] = str[i];
-			prev_is_blank = 0;
+			prev_is_char = 0;
+		}
+
+		else if (str[i] != ' ') {
+			dst[str_nb][char_nb] = str[i];
+			char_nb += 1;
+			prev_is_char = 1;
 		}
 	}
 
--- list.h
+++ list.h
@@ -1,36 +1,37 @@
-struct job_t {
-    char * title ; // Nom de la tache
-    double life ; // Duree de la tache
+#pragma once
+typedef struct {
+    char title[10] ; // Nom de la tache
+    int life ; // Duree de la tache
     int input_degree ; // Son degre de dependance
     int output_degree ; // Les taches qui en dependent
     int rank ; // Rang de latache
     int dyn_input_degree ; // Facilite de prog
     struct list_t * precedence ; // Les taches precedentes
     struct list_t * posteriority ; // Les taches ulterieures
-    double au_plus_tot ; // Date au plustot
-    double au_plus_tard ; // Date au plustard
-    double marge_totale ; // Marge totale
-    double marge_libre ; // Marge libre
-    bool critique ; // Une tache critique
-};
+    int au_plus_tot ; // Date au plustot
+    int au_plus_tard ; // Date au plustard
+    int marge_totale ; // Marge totale
+    int marge_libre ; // Marge libre
+    int critique ; // Une tache critique
+} job_t;
 
 
-struct elmlist_t {
-    struct elmlist_t * prev ;
-    struct elmlist_t * next ;
-    job_t * job ;
-}
+typedef struct element{
+    struct element* prev;
+    struct element* next;
+    job_t* jb;
+}elmlist_t;
 
-struct list_t {
-    struct elmlist_t * head ;
-    struct elmlist_t * tail ;
+typedef struct  {
+    struct element * head ;
+    struct element * tail ;
     int numelm ;
-};
+}list_t;
 
 void view_list(list_t *G,void (* view_job)(job_t *a)){
 
 }
 
-void quick_sort(list_t *G,bool (* comp)(job_t *a,job_t *b)){
+void quick_sort(list_t *G,int (* comp)(job_t *a,job_t *b)){
 
 }
